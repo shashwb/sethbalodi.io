80439988ba5ec07d46c61f9b785abb5a645f6cda
Seth Balodi <sethbalodi@gmail.com>
2022-08-26 21:39:20 -0400

    seth: created a filtered component (currently not styled). Need to connect it with other pages

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,5 +1,5 @@
 // Gatsby supports TypeScript natively!
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageProps, Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -16,94 +16,132 @@ import '../components/navbar.css';
  * > implement a tag selector at the top of the screen
  */
 
-
 /**
  * TODO: look up this typescript BS....
  */
 type PageContext = {
-    currentPage: number,
-    numPages: number;
-}
+  currentPage: number;
+  numPages: number;
+};
 type Data = {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-    allMarkdownRemark: {
-      edges: {
-        node: {
-          excerpt: string;
-          frontmatter: {
-            title: string;
-            date: string;
-            description: string;
-          };
-          fields: {
-            slug: string;
-          };
-        };
-      }[];
+  site: {
+    siteMetadata: {
+      title: string;
     };
   };
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string;
+        frontmatter: {
+          title: string;
+          date: string;
+          description: string;
+        };
+        fields: {
+          slug: string;
+        };
+      };
+    }[];
+  };
+};
 
-
-
-/** render posts  */
-
+/** render selection  */
+const CategoryFilter = ({ categories, handleChange, selected }) => {
+  return (
+    <div>
+      {categories.map((category) => (
+        <button
+          selected={selected}
+          value={category}
+          onClick={(e) => {
+            console.log('e', e);
+            console.log('cagegory', category);
+            handleChange(category);
+          }}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 /** IDEAS
  * >> maybe make this infinite scroll?
  */
 const AllContentIndex = ({ data, location, pageContext }: PageProps<Data, PageContext>) => {
-    const siteTitle = "Seth Balodi";
-    const posts = data.allMarkdownRemark.edges;
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
-    /**
-     * TODO: Create a filtering component
-     */
+  useEffect(() => {
+    console.log('selectedCategory changed!', selectedCategory);
+  }, [selectedCategory]);
+
+  /** helper functions */
+  const onHandle___selectedCategory = (category) => setSelectedCategory(category);
+
+  const siteTitle = 'Seth Balodi';
+  const posts = data.allMarkdownRemark.edges;
+
+  const getValidCategories = (posts) => {
+    // get all the categories that exist across all posts
+    const nonUniqueCategories = posts.map((post) =>
+      post.node.frontmatter.categories
+        ? post.node.frontmatter.categories.map((cat) => cat.toLowerCase())
+        : null
+    );
+    return ["all", ...new Set([].concat(...nonUniqueCategories))]; //create unique categories
+  };
+
+  const uniqueCategories = getValidCategories(posts);
 
 
-    return (
-        <Layout location={location} title={siteTitle}>
-            <SEO title="Archive" />
-            <div>
-                {renderPostsByCategory(posts, null, "Archive")}
-            </div>
-        </Layout>
-    )
-}
+  /**
+   * TODO: Create a filtering component
+   */
+
+  const filterBy = `Filter by category: ${selectedCategory}`;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Archive" />
+      <h3>Archive</h3>
+      <CategoryFilter
+        categories={uniqueCategories}
+        handleChange={onHandle___selectedCategory}
+        selected={selectedCategory}
+      />
+      <div>{renderPostsByCategory(posts, selectedCategory, filterBy)}</div>
+    </Layout>
+  );
+};
 
 export default AllContentIndex;
 
-
 /** make a GraphQL request for this page's data */
-
+// filter: { frontmatter: { categories: { in: ["essay", "programming"] } } }
 export const pageQuery = graphql`
-query contentQuery {
-  site {
-    siteMetadata {
-      title
+  query contentQuery {
+    site {
+      siteMetadata {
+        title
+      }
     }
-  }
-  allMarkdownRemark(
-    filter: { frontmatter: { categories: { in: ["essay", "programming"] } } }
-    sort: { fields: [frontmatter___date], order: DESC }
-  ) {
-    edges {
-      node {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-          categories
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            categories
+          }
         }
       }
     }
   }
-}
 `;
