faa696a0ee61c8b7f03b70ba40ad1b62df92f403
Seth Balodi <sethbalodi@gmail.com>
2022-11-05 19:40:55 -0400

    updated package.json with new naming

--- src/components/misc/Button.js
+++ src/components/misc/Button.js
@@ -3,12 +3,7 @@ import React from 'react';
 const Button = ({ isSelected, value, clickHandler }) => {
   return (
     <div className="filter-btn-element">
-      <button
-        className="clickElement"
-        onClick={() => {
-          clickHandler(value);
-        }}
-      >
+      <button className="clickElement" onClick={() => clickHandler(value)}>
         {isSelected ? <b>{`[${value}]`}</b> : value}
       </button>
     </div>
